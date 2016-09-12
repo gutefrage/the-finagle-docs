2e5f63fa389365fbe94603e43ffdfd89597082cb
Nepomuk Seiler <nepomuk.seiler@gutefrage.net>
2016-09-12 10:43:53 +0200

    Use temperature service

--- example/src/main/scala/net/gutefrage/basic/FinatraHttpServer.scala
+++ example/src/main/scala/net/gutefrage/basic/FinatraHttpServer.scala
@@ -1,36 +1,57 @@
 package net.gutefrage.basic
 
+import com.twitter.finagle.ThriftMux
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.{Controller, HttpServer}
-import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
+import com.twitter.finatra.http.filters.{
+  CommonFilters,
+  LoggingMDCFilter,
+  TraceIdMDCFilter
+}
 import com.twitter.finatra.http.routing.HttpRouter
 import com.twitter.finatra.response.Mustache
+import net.gutefrage.Dtabs
+import net.gutefrage.temperature.thrift.TemperatureService
 
 object FinatraHttpServer extends HttpServer {
+
+  premain {
+    Dtabs.init()
+  }
 
   override def defaultFinatraHttpPort = ":9000"
 
   override protected def configureHttp(router: HttpRouter): Unit =
-    router.filter[LoggingMDCFilter[Request, Response]]
-          .filter[TraceIdMDCFilter[Request, Response]]
-          .filter[CommonFilters]
-          .add[WeatherController]
+    router
+      .filter[LoggingMDCFilter[Request, Response]]
+      .filter[TraceIdMDCFilter[Request, Response]]
+      .filter[CommonFilters]
+      .add[WeatherController]
 }
 
 class WeatherController extends Controller {
 
+  val client = ThriftMux.client.newIface[TemperatureService.FutureIface](
+    dest = "/s/temperature",
+    label = "finatra-weather"
+  )
+
   get("/") { request: Request =>
-    response.ok.view(
-      "dashboard.mustache",
-      DashboardData(0.0, Some("Dashboard"))
-    )
+    client.mean().map { meanTemperature =>
+      response.ok.view(
+        "dashboard.mustache",
+        DashboardData(meanTemperature, Some("Dashboard"))
+      )
+    }
   }
 
   get("/none") { request: Request =>
-    response.ok.view(
-      "dashboard.mustache",
-      DashboardData(0.0, None)
-    )
+    client.mean().map { meanTemperature =>
+      response.ok.view(
+        "dashboard.mustache",
+        DashboardData(meanTemperature, None)
+      )
+    }
   }
 }
 
