58e3227a699f3da896c5a26ecf0e0777a4802669
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2016-09-14 20:32:21 +0200

    Use injection for temperature service

--- example/src/main/scala/net/gutefrage/basic/FinatraHttpServer.scala
+++ example/src/main/scala/net/gutefrage/basic/FinatraHttpServer.scala
@@ -1,15 +1,14 @@
 package net.gutefrage.basic
 
-import com.twitter.finagle.ThriftMux
+import javax.inject.{Inject, Singleton}
+
+import com.google.inject.{Module, Provides}
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.{Controller, HttpServer}
-import com.twitter.finatra.http.filters.{
-  CommonFilters,
-  LoggingMDCFilter,
-  TraceIdMDCFilter
-}
+import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
 import com.twitter.finatra.response.Mustache
+import com.twitter.inject.TwitterModule
 import net.gutefrage.Dtabs
 import net.gutefrage.temperature.thrift.TemperatureService
 
@@ -18,6 +17,8 @@ object FinatraHttpServer extends HttpServer {
   premain {
     Dtabs.init()
   }
+
+  override def modules: Seq[Module] = Seq(TemperatureServiceModule)
 
   override def defaultFinatraHttpPort = ":9000"
 
@@ -29,21 +30,40 @@ object FinatraHttpServer extends HttpServer {
       .add[WeatherController]
 }
 
-class WeatherController extends Controller {
+/**
+  * Provide a thrift client as an injected service
+  */
+object TemperatureServiceModule extends TwitterModule {
 
-  val client = ThriftMux.client.newIface[TemperatureService.FutureIface](
-    dest = "/s/temperature",
-    label = "finatra-weather"
-  )
+  import com.twitter.finagle.ThriftMux
+
+  @Provides
+  @Singleton
+  def provideTemperatureService(): TemperatureService.FutureIface = {
+    ThriftMux.client.newIface[TemperatureService.FutureIface](
+      dest = "/s/temperature",
+      label = "finatra-weather"
+    )
+  }
+}
+
+/**
+  * The actual controller
+  *
+  * @param temperatureService - Injected thrift client
+  */
+class WeatherController @Inject()(
+  temperatureService: TemperatureService.FutureIface
+) extends Controller {
 
   get("/") { request: Request =>
-    client.mean().map { meanTemperature =>
+    temperatureService.mean().map { meanTemperature =>
       DashboardData(meanTemperature, Some("Dashboard"))
     }
   }
 
   get("/none") { request: Request =>
-    client.mean().map { meanTemperature =>
+    temperatureService.mean().map { meanTemperature =>
       response.ok.view(
         "dashboard.mustache",
         DashboardData(meanTemperature, None)
